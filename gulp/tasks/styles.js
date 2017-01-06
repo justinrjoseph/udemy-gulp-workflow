0e68a6c8e6c97c1a78ef1c280afee3ce7786ce47
Justin Joseph <justin.joseph0@yahoo.com>
2017-01-05 21:05:10 -0500

    Reorganize gulpfile

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+    postCss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    variables = require('postcss-simple-vars'),
+    nesting = require('postcss-nested'),
+    cssImporting = require('postcss-import'),
+    mixins = require('postcss-mixins');
+
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+             .pipe(postCss([cssImporting, variables, mixins, nesting, autoprefixer]))
+             .pipe(gulp.dest('./app/temp/styles'));
+});
