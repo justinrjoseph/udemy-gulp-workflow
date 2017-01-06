225e4663662f7f3d20c23661b464b4bb4475bbb0
Justin Joseph <justin.joseph0@yahoo.com>
2017-01-05 20:58:55 -0500

    Add and implement Browsersync package

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var gulp = require('gulp'),
     variables = require('postcss-simple-vars'),
     nesting = require('postcss-nested'),
     cssImporting = require('postcss-import'),
-    mixins = require('postcss-mixins');
+    mixins = require('postcss-mixins'),
+    browserSync = require('browser-sync').create();
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
@@ -13,10 +14,26 @@ gulp.task('styles', function() {
              .pipe(gulp.dest('./app/temp/styles'));
 });
 
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+             .pipe(browserSync.stream());
+});
+
 gulp.task('watch', function() {
 
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+
   watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('styles');
+    gulp.start('cssInject');
   });
 
 });
